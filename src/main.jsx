b8f9b10a9fcdd0db1412baca78d05c4c817532c7
alexkuehnl <kuehnla2@gmail.com>
2024-04-05 00:45:58 -0500

    Create Display component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Input from './components/Input'
+import Display from './components/Display'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Input />
+    <Display />
   </React.StrictMode>,
 )
